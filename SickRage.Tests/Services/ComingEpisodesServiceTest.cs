977ce715b7365afd5744f283b0ffffeaead09bb5
Preben Huybrechts <hpreben@gmail.com>
2015-01-24 22:02:41 +0100

    Added missing test file

---
+++ SickRage.Tests/Services/ComingEpisodesServiceTest.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SickRage.Model;
+
+namespace SickRage.Tests.Services
+{
+    [TestClass]
+    public class ComingEpisodesServiceTest : BaseServiceTest
+    {
+        [TestClass]
+        public class ByDate : ComingEpisodesServiceTest
+        {
+            [TestMethod]
+            public void ReturnsEpisodes()
+            {
+                //Act
+                var comingEpisodes = Client.ComingEpisodes.ByDate();
+
+                //Assert
+                Assert.IsTrue(comingEpisodes.Count > 0);
+            }
+
+            [TestMethod]
+            public void ReturnsAllTypes()
+            {
+                //Act
+                var comingEpisodes = Client.ComingEpisodes.ByDate();
+
+                //Assert
+                Assert.IsTrue(comingEpisodes.ContainsKey(FutureType.Later));
+                Assert.IsTrue(comingEpisodes.ContainsKey(FutureType.Missed));
+                Assert.IsTrue(comingEpisodes.ContainsKey(FutureType.Soon));
+                Assert.IsTrue(comingEpisodes.ContainsKey(FutureType.Today));
+            }
+
+            [TestMethod]
+            public void WithSoon_OnlyReturnsSoon()
+            {
+                //Act
+                var comingEpisodes = Client.ComingEpisodes.ByDate(FutureType.Soon);
+
+                //Assert
+                Assert.IsTrue(comingEpisodes.ContainsKey(FutureType.Soon));
+                Assert.IsFalse(comingEpisodes.ContainsKey(FutureType.Missed));
+                Assert.IsFalse(comingEpisodes.ContainsKey(FutureType.Later));
+                Assert.IsFalse(comingEpisodes.ContainsKey(FutureType.Today));
+            }
+        }
+
+        [TestClass]
+        public class ByNetwork : ComingEpisodesServiceTest
+        {
+            [TestMethod]
+            public void ReturnsEpisodes()
+            {
+                //Act
+                var comingEpisodes = Client.ComingEpisodes.ByNetwork();
+
+                //Assert
+                Assert.IsTrue(comingEpisodes.Count > 0);
+            }
+
+            [TestMethod]
+            public void WithLater_OnlyReturnsLater()
+            {
+                //Act
+                var comingEpisodes = Client.ComingEpisodes.ByDate(FutureType.Later);
+
+                //Assert
+                Assert.IsTrue(comingEpisodes.ContainsKey(FutureType.Later));
+                Assert.IsFalse(comingEpisodes.ContainsKey(FutureType.Missed));
+                Assert.IsFalse(comingEpisodes.ContainsKey(FutureType.Soon));
+                Assert.IsFalse(comingEpisodes.ContainsKey(FutureType.Today));
+            }
+        }
+
+        [TestClass]
+        public class ByShowName : ComingEpisodesServiceTest
+        {
+            [TestMethod]
+            public void ReturnsEpisodes()
+            {
+                //Act
+                var comingEpisodes = Client.ComingEpisodes.ByShowName();
+
+                //Assert
+                Assert.IsTrue(comingEpisodes.Count > 0);
+            }
+
+            [TestMethod]
+            public void WithMissed_OnlyReturnsMissed()
+            {
+                //Act
+                var comingEpisodes = Client.ComingEpisodes.ByDate(FutureType.Missed);
+
+                //Assert
+                Assert.IsTrue(comingEpisodes.ContainsKey(FutureType.Missed));
+                Assert.IsFalse(comingEpisodes.ContainsKey(FutureType.Later));
+                Assert.IsFalse(comingEpisodes.ContainsKey(FutureType.Soon));
+                Assert.IsFalse(comingEpisodes.ContainsKey(FutureType.Today));
+            }
+        }
+    }
+}
