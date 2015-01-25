06f2658bc2d64693744cbbbaaa821a4eba4be9ff
Preben Huybrechts <hpreben@gmail.com>
2015-01-25 14:54:16 +0100

    nuspec without version

--- SickRage/Properties/AssemblyInfo.cs
+++ SickRage/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SickRage")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A library to consume the sickrage api from within a .net application.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Pregress")]
 [assembly: AssemblyProduct("SickRage")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
