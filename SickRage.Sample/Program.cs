e1bf8f84eeab1b27abd935a76c9ec86f3e7740ca
Preben Huybrechts <hpreben@gmail.com>
2015-01-25 15:24:38 +0100

    Added sample project

---
+++ SickRage.Sample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SickRage.Sample
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string apiKey = "-REPLACE-BY-YOUR-API-KEY-";
+            string url = "http://192.168.0.200:8083"; //Replace by your sickrage location
+            var client = new Client(url, apiKey);
+
+            var shows = client.Show.GetShows();
+
+            foreach (var show in shows)
+            {
+                Console.WriteLine(show);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
